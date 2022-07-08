94225ce9ffe607997171f2b08a77c2878ecb2bfd
umutarpat <arptumut@gmail.com>
2022-07-08 16:44:22 +0300

    added multiple data fetching, cacheable futurebuilder structure

---
+++ lib/global/services/food/food_service.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_food_app/global/models/food_category_model/food_category_model.dart';
+import 'package:flutter_food_app/global/utils/logger.dart';
+
+Future<List<FoodCategoryModel>> getCategoriesService() async {
+  final snapshot =
+      await FirebaseFirestore.instance.collection('food-category').get();
+  logger(snapshot.docs.map((doc) => doc.data()));
+  List<FoodCategoryModel> list = snapshot.docs
+      .map((doc) => FoodCategoryModel.fromJson(doc.data()))
+      .toList();
+  return list;
+}
--- lib/modules/home/controllers/home_controller.dart
+++ lib/modules/home/controllers/home_controller.dart
@@ -1,19 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_food_app/global/models/food_category_model/food_category_model.dart';
+import 'package:flutter_food_app/global/services/food/food_service.dart';
 import 'package:flutter_food_app/global/utils/logger.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+  final mainFuture = Future.wait([]).obs;
+  RxList<FoodCategoryModel> foodCategoryData = RxList();
+
+  late final Future<List<FoodCategoryModel>> foodCategoryFuture;
+
+  // final foodCategoryFuture = Future.value(List<FoodCategoryModel>).obs;
+
   @override
   void onInit() {
     super.onInit();
-    getCategories();
+    updateMainFuture();
   }
 
-  getCategories() async {
-    final snapshot =
-        await FirebaseFirestore.instance.collection('food-category').get();
-    logger(snapshot.docs.map((doc) => doc.data()));
+  updateMainFuture() {
+    getCategories();
+    mainFuture.value = Future.wait([foodCategoryFuture]);
+    update(['main']);
+  }
+
+  assignFutures(List data) {
+    final datas = [foodCategoryData];
+    data.forEach((element) {
+      datas[data.indexOf(element)].value = element;
+    });
+  }
+
+  getCategories() {
+    foodCategoryFuture = getCategoriesService();
   }
 }
